738d144f3fe8a6dd827fe822343a01ed1b0ddea3
artemnaplavkov <74201877+artemnaplavkov@users.noreply.github.com>
2025-02-28 16:43:56 +0300

    Update lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -5,53 +5,46 @@ using namespace std;
 
 
 struct List {
-	struct Node{
-		float val;
-		Node* next= nullptr;
+    float val;
+	List* next= nullptr;
+};
+
+void free(List* head) {
+	while (head) {
+		List* tmp = head;
+		head = head->next;
+		delete tmp;
 	};
-	Node* head=nullptr;
-	Node* tail= nullptr;
-	void push_back(float el) {
+}
+
+List* read(istream&in) {
+	List* head = nullptr;
+	List* tail = nullptr;
+	for (float el; in >> el && !in.eof() && !in.fail();) {
 		if (tail) {
-			tail->next = new Node{};
+			tail->next = new List{};
 			tail = tail->next;
 		}
 		else {
-			tail = new Node{};
+			tail = new List{};
 			head = tail;
 		}
 		tail->val = el;
 	}
-	void free() {
-		Node* ptr = head;
-		while (ptr) {
-			Node* tmp = ptr;
-			ptr = ptr->next;
-			delete tmp;
-		}
-		head = nullptr;
-		tail = nullptr;
-	}
-};
-
-void read(istream&in, List&out) {
-	for (float el; in >> el && !in.eof() && !in.fail();) {
-		out.push_back(el);
-	}
+	return head;
 }
 
-void print(List& list) {
-	List::Node* ptr = list.head;
-	while (ptr) {
-		cout << ptr->val << " ";
-		ptr = ptr->next;
+void print(List* head) {
+	for (;  head; head = head->next) {
+		cout << head->val << " ";
 	}
 	cout << "\n";
 }
 
-void stable_partition(List&list) {
-	List::Node* j = list.head;
-	for (List::Node* i = list.head; i; i = i->next) {
+void stable_partition(List* head) {
+	List* i = head;
+	List* j = head;
+	for (; i; i = i->next) {
 		if (i->val!=0) {
 			j->val = i->val;
 			j = j->next;
@@ -62,25 +55,23 @@ void stable_partition(List&list) {
 	}
 }
 
-
 int main(int argc, char *argv[]) {
 	int ask;
 	cout << "Insert 0: keyboard; 1: file\n";
 	cin >> ask;
-	
 
-	List list;
+	List* head = nullptr;
 	if (ask) {
 		ifstream in("C:\\Users\\79648\\Downloads\\lab1_newsubject_final\\lab.txt");
-		read(in, list);
+		head = read(in);
 	}
 	else {
-		read(cin,list);
+		head = read(cin);
 	}
-	print(list);
-	stable_partition(list);
-	print(list);
-	list.free();
+	print(head);
+	stable_partition(head);
+	print(head);
+	free(head);
 
 	cout << "done\n";
 }
